c1b5ccb7424d2dfa875081838f4b8f2cc94c8ebc
Dalton Perdue <dalton@perdue.io>
2026-07-01 17:28:31 -0400

    chore: use renamed module path in sweep controller

--- internal/controller/sweep_controller.go
+++ internal/controller/sweep_controller.go
@@ -28,8 +28,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	giteaactionsv1alpha1 "github.com/blackrabbit/gitea-act-runner-controller/api/v1alpha1"
-	"github.com/blackrabbit/gitea-act-runner-controller/internal/gitea"
+	giteaactionsv1alpha1 "github.com/f33rx/gitea-act-runner-controller/api/v1alpha1"
+	"github.com/f33rx/gitea-act-runner-controller/internal/gitea"
 )
 
 // SweepReconciler periodically sweeps for orphaned Gitea runners whose pods are gone.
